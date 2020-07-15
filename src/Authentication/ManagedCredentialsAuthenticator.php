b38a8a12b78a667564c905c6c0906c53997aadd1
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2020-07-15 15:46:08 +0200

    Update ManagedCredentialsAuthenticator.php

--- src/Authentication/ManagedCredentialsAuthenticator.php
+++ src/Authentication/ManagedCredentialsAuthenticator.php
@@ -29,7 +29,7 @@ class ManagedCredentialsAuthenticator implements AuthenticatorInterface
         try {
             $client = $this->clientFactory->getClient(self::INSTANCE_METADATA_SERVICE_ENDPOINT);
             $response = $client->get(
-                sprintf('/metadata/identity/oauth2/token?api-version=%s&format=text', self::API_VERSION),
+                sprintf('/metadata/identity/oauth2/token?api-version=%s&format=text&resource=https://vault.azure.net', self::API_VERSION),
                 [
                     'headers' => [
                         'Metadata' => 'true'
@@ -52,11 +52,18 @@ class ManagedCredentialsAuthenticator implements AuthenticatorInterface
         try {
             $client = $this->clientFactory->getClient(
                 self::INSTANCE_METADATA_SERVICE_ENDPOINT,
-                ['backoffMaxRetries' => 1]
+                ['backoffMaxTries' => 2]
             );
-            $client->get(sprintf('/metadata?api-version=%s&format=text', self::API_VERSION));
-        } catch (GuzzleException $e) {
-            throw new ClientException('Instance metadata service not available: ' . $e->getMessage(), 0, $e);
-        }
+            $client->get(
+                sprintf('/metadata?api-version=%s&format=text', self::API_VERSION), 
+                [
+                    'headers' => [
+                        'Metadata' => 'true'
+                    ],
+                ]
+
+            );
+	} catch (GuzzleException $e) {
+	    throw new ClientException('Instance metadata service not available: ' . $e->getMessage(), 0, $e);
     }
 }
